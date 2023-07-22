7151d4bb78262dddfae546ecde9762c53fcd9ac6
Aliaksei Miadzvetski <123022357+GhosteLLoS@users.noreply.github.com>
2023-07-22 22:12:38 +0200

    Add files via upload

---
+++ RabbitMQ/model.py
@@ -0,0 +1,9 @@
+from mongoengine import Document
+from mongoengine.fields import StringField, BooleanField
+
+
+class Messages(Document):
+    fullname = StringField()
+    email = StringField()
+    processed = BooleanField(default=False)
+    content = StringField()
